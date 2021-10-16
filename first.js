2d463fb7771d8eda7391dda569a2f0c52f540d49
QasemGit <d.nnnn.8@gmail.com>
2021-10-16 18:20:20 +0300

    first project

---
+++ first.js
@@ -0,0 +1,7 @@
+function genpic(){
+var image =document.createElement('img');
+var div=document.getElementById('flex-gen');
+image.src = "https://i.pinimg.com/564x/7d/35/6c/7d356c6de23bddfc0e56a83c581d96af.jpg"
+div.appendChild(image);
+}
+
